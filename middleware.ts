a1361d71a204a63449ea4d9aad966bce35767424
swermers <shayne.wermers@gmail.com>
2026-02-06 21:20:20 +0100

    commit

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { type NextRequest } from 'next/server';
 import { updateSession } from '@/utils/supabase/middleware';
 
 export async function middleware(request: NextRequest) {
+//  return await updateSession(request);
 return NextResponse.next();
 }
 
