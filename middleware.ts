0d7fcef8d7dbee1ff285839648a1b2c686aa77f4
swermers <shayne.wermers@gmail.com>
2026-02-06 21:21:32 +0100

    commit

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,13 @@
-import { type NextRequest } from 'next/server';
+import { type NextRequest, NextResponse } from 'next/server'; // Import NextResponse
 import { updateSession } from '@/utils/supabase/middleware';
 
 export async function middleware(request: NextRequest) {
-//  return await updateSession(request);
-return NextResponse.next();
+  // return await updateSession(request); <--- COMMENT THIS OUT
+  return NextResponse.next(); // <--- ADD THIS (Let everyone in!)
 }
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
-     */
     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 };
