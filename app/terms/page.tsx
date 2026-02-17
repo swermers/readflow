d83e911b04eb193ff8568700cef44dbb2b60c469
swermers <shayne.wermers@gmail.com>
2026-02-17 15:21:55 +0100

    Update page.tsx

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -24,7 +24,9 @@ export default function TermsPage() {
             revised terms.
           </p>
         </section>
-        ;)
-        
+ </div> {/* Add this closing div */}
+    </main>
+  );
+} 
 
 }
